3eb2621f098f906ecb0a8933256fc42ea9eb6349
SarNadya <lutik.ne@gmail.com>
2023-04-24 20:46:43 +0300

    добавила анимации из библиотеки Framer motion

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -1,22 +1,110 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
 import './Description.css';
-import img from './images/IMAGE.png';
+import { motion, useScroll } from 'framer-motion'
 
 function Description() {
+  const { scrollYProgress } = useScroll();
   return (
     <div className='wrap'>
-      <Card style={{ width: '300px', margin: '0 auto' }}>
-        <Card.Img variant="top" src={img} />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Why New York is still our favorite city to visit
-            </Card.Text>
-            <Button variant="primary">Read more</Button>
-        </Card.Body>
-      </Card>
+      <motion.div
+        className="box"
+        animate={{
+          scale: [1, 2, 2, 1, 1],
+          rotate: [0, 0, 180, 180, 0],
+          borderRadius: ["0%", "0%", "50%", "50%", "0%"]
+        }}
+        transition={{
+          duration: 2,
+          ease: "easeInOut",
+          times: [0, 0.2, 0.5, 0.8, 1],
+          repeat: Infinity,
+          repeatDelay: 1
+        }}
+      />
+      <div>
+        <motion.div
+          className="progress-bar"
+          style={{ scaleX: scrollYProgress }}
+        />
+        <h1>
+          <code>useScroll</code> demo
+        </h1>
+        <article>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ac
+          rhoncus quam.
+        </p>
+        <p>
+          Fringilla quam urna. Cras turpis elit, euismod eget ligula quis,
+          imperdiet sagittis justo. In viverra fermentum ex ac vestibulum.
+          Aliquam eleifend nunc a luctus porta. Mauris laoreet augue ut felis
+          blandit, at iaculis odio ultrices. Nulla facilisi. Vestibulum cursus
+          ipsum tellus, eu tincidunt neque tincidunt a.
+        </p>
+        <h2>Sub-header</h2>
+        <p>
+          In eget sodales arcu, consectetur efficitur metus. Duis efficitur
+          tincidunt odio, sit amet laoreet massa fringilla eu.
+        </p>
+        <p>
+          Pellentesque id lacus pulvinar elit pulvinar pretium ac non urna.
+          Mauris id mauris vel arcu commodo venenatis. Aliquam eu risus arcu.
+          Proin sit amet lacus mollis, semper massa ut, rutrum mi.
+        </p>
+        <p>Sed sem nisi, luctus consequat ligula in, congue sodales nisl.</p>
+        <p>
+          Vestibulum bibendum at erat sit amet pulvinar. Pellentesque pharetra
+          leo vitae tristique rutrum. Donec ut volutpat ante, ut suscipit leo.
+        </p>
+        <h2>Sub-header</h2>
+        <p>
+          Maecenas quis elementum nulla, in lacinia nisl. Ut rutrum fringilla
+          aliquet. Pellentesque auctor vehicula malesuada. Aliquam id feugiat
+          sem, sit amet tempor nulla. Quisque fermentum felis faucibus, vehicula
+          metus ac, interdum nibh. Curabitur vitae convallis ligula. Integer ac
+          enim vel felis pharetra laoreet. Interdum et malesuada fames ac ante
+          ipsum primis in faucibus. Pellentesque hendrerit ac augue quis
+          pretium.
+        </p>
+        <p>
+          Morbi ut scelerisque nibh. Integer auctor, massa non dictum tristique,
+          elit metus efficitur elit, ac pretium sapien nisl nec ante. In et ex
+          ultricies, mollis mi in, euismod dolor.
+        </p>
+        <p>Quisque convallis ligula non magna efficitur tincidunt.</p>
+        <p>
+          Pellentesque id lacus pulvinar elit pulvinar pretium ac non urna.
+          Mauris id mauris vel arcu commodo venenatis. Aliquam eu risus arcu.
+          Proin sit amet lacus mollis, semper massa ut, rutrum mi.
+        </p>
+        <p>Sed sem nisi, luctus consequat ligula in, congue sodales nisl.</p>
+        <p>
+          Vestibulum bibendum at erat sit amet pulvinar. Pellentesque pharetra
+          leo vitae tristique rutrum. Donec ut volutpat ante, ut suscipit leo.
+        </p>
+        <h2>Sub-header</h2>
+        <p>
+          Maecenas quis elementum nulla, in lacinia nisl. Ut rutrum fringilla
+          aliquet. Pellentesque auctor vehicula malesuada. Aliquam id feugiat
+          sem, sit amet tempor nulla. Quisque fermentum felis faucibus, vehicula
+          metus ac, interdum nibh. Curabitur vitae convallis ligula. Integer ac
+          enim vel felis pharetra laoreet. Interdum et malesuada fames ac ante
+          ipsum primis in faucibus. Pellentesque hendrerit ac augue quis
+          pretium.
+        </p>
+        <p>
+          Morbi ut scelerisque nibh. Integer auctor, massa non dictum tristique,
+          elit metus efficitur elit, ac pretium sapien nisl nec ante. In et ex
+          ultricies, mollis mi in, euismod dolor.
+        </p>
+        <p>Quisque convallis ligula non magna efficitur tincidunt.</p>
+      </article>
+      </div>
+      <div className="example-container">
+        <motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }}>
+          <a href="#" className="link">Наверх</a>
+        </motion.div>
+      </div>
     </div>
   )
 }
